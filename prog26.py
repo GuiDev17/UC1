07f3cab516241382a4592afcc0781187ac74703c
GuiDev17 <guilhermecosta1706@gmail.com>
2026-07-06 20:42:50 -0300

    prog26.py

---
+++ prog26.py
@@ -0,0 +1,8 @@
+v1=int(input("Digite um valor: "))
+v2=int(input("Digite outro valor: "))
+if v1>v2:
+    print(f"O valor {v1} é maior que o valor {v2}")
+elif v2>v1:
+    print(f"O valor {v2} é maior que o valor {v1}")
+else:
+    print("Os valores são iguais.")
