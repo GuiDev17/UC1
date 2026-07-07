898a020091a9bed73592064829bd9b6db9c5a7b4
GuiDev17 <guilhermecosta1706@gmail.com>
2026-07-06 21:18:37 -0300

    Restore calculator logic and add test print
    Restored calculator functionality and added a test print statement.

--- prog25.py
+++ prog25.py
@@ -20,4 +20,5 @@ match op:
         t=0
         print("Operação Inválida")
 print(f"O resultado da operação é {t}")
-print("teste de commit")        
+print("teste de commit")
+print("teste de pull")
